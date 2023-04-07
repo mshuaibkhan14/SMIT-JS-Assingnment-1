b5d5f169864ca8e7b8d498e4434f7eab4fcac3d9
mshuaibkhan14 <129395511+mshuaibkhan14@users.noreply.github.com>
2023-04-07 19:37:05 +0500

    Add files via upload

---
+++ SMIT JS ASSINGNMENT 1/app.js
@@ -0,0 +1,131 @@
+// alert ("Helllo SMIT")
+
+
+/////////////////// Question 1 ///////////////////
+
+let num1 = +prompt ("Enter a number");
+let num2 = +prompt ("Enter a number");
+
+if (num1 > num2) {
+    console.log(num1);
+}
+  else {
+    console.log(num2);
+ }
+
+
+
+
+
+/////////////////// Question 2 ///////////////////
+
+// let num = +prompt ("Enter a number positive or Nagative");
+
+// if (num >= 0 ) {
+//     console.log("The Number is +");
+// }
+//   else {
+//     console.log("The Number is -");
+//  }
+
+
+
+
+
+ /////////////////// Question 3 ///////////////////
+
+//  let num1 = +prompt ("Enter a number");
+//  let num2 = +prompt ("Enter a number");
+//  let num3 = +prompt ("Enter a number");
+//  let num4 = +prompt ("Enter a number");
+//  let num5 = +prompt ("Enter a number");
+
+//  let largNum = Math.max(num1, num2, num3, num4, num5);
+
+//  console.log("The largest number is: " + largNum);
+
+
+
+
+
+/////////////////// Question 4 ///////////////////
+
+
+// for (let i = 0; i <= 15; i++) {
+//    let numValue = i;
+
+// if(numValue % 2 == 0) {
+//     console.log(numValue + " is even.");
+// }
+
+// else {
+//     console.log(numValue + " is odd.");
+// }
+
+// }
+
+
+
+
+
+/////////////////// Question 5 ///////////////////
+
+
+
+
+// let stdMarks = +prompt("Enter your marks");
+
+// if (stdMarks < 60){
+//     console.log("Grade : F");      
+//     }  
+
+//   else if (stdMarks < 70) {
+//           console.log("Grade : D"); 
+//   }
+
+//   else if (stdMarks < 80){
+//           console.log("Grade : C"); 
+//   }
+
+//    else if (stdMarks < 90) {
+//           console.log("Grade : B"); 
+//   }
+
+//   else if (stdMarks <= 100) {
+//           console.log("Grade : A"); 
+//   }
+//     else if (stdMarks  > 100){
+//         console.log("Plase enter valid marks")
+//   }
+ 
+
+
+
+    
+/////////////////// Question 6 ///////////////////
+
+// for (let i = 1; i <= 100; i++) {
+
+//     if (i % 15 ==0 ) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
+
+
+
+
+
+
+
+/////////////////// Question 7 ///////////////////
+
+
+// let star = "";
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 0; j < i; j++) {
+//     star += "*";
+//   }
+//   star += "\n";
+// }
+// console.log(star);
